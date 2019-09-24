51c7640775a47d13747874ccf41c66fc60579886
Daniel Sinclair <d@niel.nyc>
2019-09-24 19:30:17 -0400

    fixed syntax errors and added single/multiple line optionality

--- runway_model.py
+++ runway_model.py
@@ -1,16 +1,19 @@
 import runway
 from cnocr import CnOcr
 
-@runway.setup(options={"network_size":category(choices=[64, 128, 256, 512])
-def setup(opts):
-    model = CnOcr()
-    return model
 
-@runway.command('classify', inputs={'image':runway.file(description='File path')}, outputs={'text':runway.text(description='Text')})
+@runway.setup(options={'lines': runway.category(choices=['single', 'multiple'])})
+def setup(opts):
+    ocr = CnOcr()
+    return ocr.ocr if opts['lines'] == 'multiple' else ocr.ocr_for_single_line
+
+
+@runway.command('classify', inputs={'image': runway.file(description='File path')}, outputs={'text': runway.text(description='Text')})
 def classify(model, inputs):
-    res = model.ocr(inputs['image'])
+    res = model(inputs['image'])
     print(res)
-    return {'text':res}
+    return {'text': res}
+
 
 if __name__ == '__main__':
     runway.run(host='0.0.0.0', port=9000)
