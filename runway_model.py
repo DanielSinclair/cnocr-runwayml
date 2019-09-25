d5748359cdd7e12a2a998ae2c05f16db582d30d0
Cristobal Valenzuela <cris@runwayml.com>
2019-09-25 12:07:23 -0400

    fix types

--- runway_model.py
+++ runway_model.py
@@ -1,4 +1,5 @@
 import runway
+import numpy as np
 from cnocr import CnOcr
 
 @runway.setup(options={"network_size":runway.category(choices=['64', '128', '256', '512'])})
@@ -6,10 +7,10 @@ def setup(opts):
     model = CnOcr()
     return model
 
-@runway.command('classify', inputs={'image': runway.image}, outputs={'image': runway.text})
+@runway.command('classify', inputs={'image': runway.image}, outputs={'text': runway.text})
 def classify(model, inputs):
-    res = model.ocr(inputs['image'])
-    print(res)
+    image = np.array(inputs['image'])
+    res = model.ocr(image)
     return {'text':res}
 
 if __name__ == '__main__':
