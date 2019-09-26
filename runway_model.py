cbbfc2f6e9044aaba51ab19851624d239c194e82
Daniel Sinclair <d@niel.nyc>
2019-09-26 03:57:31 -0400

    operational model with input descriptions

--- runway_model.py
+++ runway_model.py
@@ -1,19 +1,29 @@
 import runway
+import numpy
 from cnocr import CnOcr
 
+lineCategory = runway.category(
+    description = "Number of lines of characters. Improves accurary.",
+    choices = ['single', 'multiple'], 
+    default = 'multiple'
+)
 
-@runway.setup(options={'lines': runway.category(choices=['single', 'multiple'])})
+@runway.setup(options={'lines': lineCategory})
 def setup(opts):
-    ocr = CnOcr()
-    return ocr.ocr if opts['lines'] == 'multiple' else ocr.ocr_for_single_line
+    model = CnOcr()
+    return model.ocr if opts['lines'] == 'multiple' else model.ocr_for_single_line
 
+classifyInput = {'image': runway.image(description="Image with Chinese characters.")}
+classifyOutput = {'characters': runway.array(
+    item_type = runway.array(item_type = runway.text), 
+    description = "2D character array."
+)}
 
-@runway.command('classify', inputs={'image': runway.file(description='File path')}, outputs={'text': runway.text(description='Text')})
+@runway.command('classify', inputs = classifyInput, outputs = classifyOutput)
 def classify(model, inputs):
-    res = model(inputs['image'])
-    print(res)
-    return {'text': res}
-
+    image = numpy.array(inputs['image'])
+    res = model(image)
+    return {'characters': res}
 
 if __name__ == '__main__':
     runway.run(host='0.0.0.0', port=9000)
